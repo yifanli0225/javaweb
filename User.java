fde78e09fb8bcebba7e7a3f647b9be56cbeb64f5
yifanli0225 <56816862+yifanli0225@users.noreply.github.com>
2019-10-21 15:04:59 +0800

    新增一个User.java

---
+++ User.java
@@ -0,0 +1,38 @@
+package entity;
+
+public class User {
+    private String userName;
+    private String pwd;
+
+    public User() {
+    }
+
+    public User(String userName, String pwd) {
+        this.userName = userName;
+        this.pwd = pwd;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+}
